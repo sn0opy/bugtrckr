2ede6b49a5a867c6249ff4561f82eb3bd58f6115
Philipp Hirsch <philipp-hirsch@gmx.net>
2011-04-09 13:53:00 +0200

    asdf

--- gui/home.tpl.php
+++ gui/home.tpl.php
@@ -2,7 +2,7 @@
 
 <p>
 Bugtrckr is a minimal but feature rich Bugtracking System.
-It's build in PHP and uses the Fat Free Framework.
+It's programmed with PHP and the Fat Free Framework.
 </p>
 
 <p>Created by sn0opy &amp; Hanspolo</p>
