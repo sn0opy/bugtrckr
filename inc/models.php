cd9578765cec605d8a7a9ef6057ff9841a62c69f
Philipp Hirsch <philipp-hirsch@gmx.net>
2011-05-12 20:41:13 +0200

    Port to Axon started

---
+++ inc/models.php
@@ -0,0 +1,15 @@
+<?php
+
+	class Activity extends Axon {}
+
+	class Milestone extends Axon {}
+
+	class Project extends Axon {}
+
+	class ProjectPermissions extends Axon {}
+
+	class Role extends Axon {}
+
+	class Ticket extends Axon {}
+
+	class User extends Axon {}
