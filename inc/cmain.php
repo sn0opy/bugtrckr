2e9dfe7a76ec2236009d40a2664231b32a47dfef
Philipp Hirsch <philipp-hirsch@gmx.net>
2011-09-01 15:37:15 +0200

    merge

--- inc/cmain.php
+++ inc/cmain.php
@@ -29,7 +29,7 @@ class cmain extends Controller
     {
         $url = $this->get('SERVER.HTTP_REFERER');
         $projectId = ($hash) ? $hash : $this->get('REQUEST.project');
-        
+
         if($projectId == 'new') 
             $this->reroute($this->get('BASE').'/project/add');
 
