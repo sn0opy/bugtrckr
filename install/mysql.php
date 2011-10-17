ec310238b607d63b2b871556af8bb2bfc1579b61
Sascha Ohms <sasch9r@gmail.com>
2011-10-17 23:44:28 +0200

    mysql should work again

--- install/mysql.php
+++ install/mysql.php
@@ -1,30 +1,32 @@
 <?php
 
-
-DB::sql('CREATE TABLE Ticket (id INTEGER PRIMARY KEY AUTO_INCREMENT, hash VARCHAR(12), title varchar(40), description text, created int, owner int, assigned int, type int, state int, priority int, category int, milestone int);');
-DB::sql('CREATE TABLE User (id integer PRIMARY KEY AUTO_INCREMENT, name VARCHAR(45), hash varchar(12), password varchar(40), salt varchar(5), email varchar(50), admin int(1), lastProject int(11));');
+DB::sql('CREATE TABLE Ticket2 (hash CHAR(12) PRIMARY KEY, title VARCHAR(40), description text, created date, owner CHAR(12), assigned CHAR(12), type int, state int, priority int, category CHAR(12), milestone CHAR(12));');
+DB::sql('CREATE TABLE User (name VARCHAR(40), hash CHAR(12) PRIMARY KEY, password CHAR(32), salt CHAR(32), email VARCHAR(40), admin bool, lastProject CHAR(12));');
 DB::sql('CREATE TABLE Project (id INTEGER PRIMARY KEY AUTO_INCREMENT, hash VARCHAR(12), name VARCHAR(45), description TEXT, public int(1));');
-DB::sql('CREATE TABLE Milestone (id INTEGER PRIMARY KEY AUTO_INCREMENT, hash VARCHAR(12), name varchar(45), description text, finished date, project int(11));');
-DB::sql('CREATE TABLE Activity (id INTEGER PRIMARY KEY AUTO_INCREMENT, hash VARCHAR(12), description text, user int(11), changed int(11), project int(11), ticket int(11), comment TEXT);');
-DB::sql('CREATE TABLE Role (id INTEGER PRIMARY KEY AUTO_INCREMENT , projectId int(11), hash VARCHAR(12), name VARCHAR(45), issuesAssigneable int(1), proj_editProject int(1), proj_manageMembers int(1), proj_manageRoles int(1), iss_editIssues int(1), iss_addIssues int(1), iss_deleteIssues int(1), iss_moveIssue int(1), iss_editWatchers int(1), iss_addWatchers int(1), iss_viewWatchers int(1));');
-DB::sql('CREATE TABLE ProjectPermission (userId INTEGER, projectId INTEGER, roleId INTEGER, PRIMARY KEY (userId, projectId));');
-DB::sql('CREATE TABLE ProjectAdmins (id INTEGER PRIMARY KEY AUTO_INCREMENT, userID INTEGER, projectID INTEGER);');
-DB::sql('CREATE TABLE Category( id INTEGER PRIMARY KEY, name VARCHAR(20), projectId INT(11));');
+DB::sql('CREATE TABLE Milestone (hash CHAR(12) PRIMARY KEY, name VARCHAR(40), description text, finished date, project CHAR(12));');
+DB::sql('CREATE TABLE Activity (hash CHAR(12) PRIMARY KEY, description text, user CHAR(12), changed date, project CHAR(12), ticket CHAR(12), comment TEXT);');
+DB::sql('CREATE TABLE Role (project CHAR(12), hash CHAR(12), name VARCHAR(40), issuesAssigneable BOOL, proj_editProject BOOL, proj_manageMembers BOOL, proj_manageRoles BOOL, iss_editIssues BOOL, iss_addIssues BOOL, iss_deleteIssues BOOL, iss_moveIssue BOOL, iss_editWatchers BOOL, iss_addWatchers BOOL, iss_viewWatchers BOOL);');
+DB::sql('CREATE TABLE ProjectPermission (user CHAR(12), project CHAR(12), role CHAR(12), PRIMARY KEY (user, project));');
+DB::sql('CREATE TABLE ProjectAdmins (hash CHAR(12) PRIMARY KEY, user CHAR(12), project CHAR(12));');
+DB::sql('CREATE TABLE Category (hash CHAR(12) PRIMARY KEY, project CHAR(12), name VARCHAR(40));');
+DB::sql('CREATE TABLE WikiEntry (hash CHAR(32) PRIMARY KEY, title VARCHAR(30), content TEXT, project CHAR(32), created DATE, created_by CHAR(32), edited DATE, edited_by CHAR(32));');
 
 DB::sql('CREATE VIEW user_perms as SELECT * FROM user, projectpermission WHERE user.id = projectpermission.userId;');
 DB::sql('CREATE VIEW user_ticket AS SELECT user.hash as userhash, ticket.hash as tickethash FROM user, ticket WHERE user.id = ticket.owner;');
 
 DB::sql('CREATE VIEW displayableticket AS 
-    SELECT Priority.name as priorityname, Status.name as statusname, Type.name as typename, Owner.hash as userhash, Owner.name as username, Ticket.hash as tickethash, Assigned.hash as assignedhash, Assigned.name as assignedname, Category.name as categoryname, Ticket.* 
-    FROM User as Owner, Status, Priority, Type, Category, Ticket 
-    LEFT OUTER JOIN User as Assigned ON Assigned.id = Ticket.assigned 
-    WHERE Owner.id = Ticket.owner AND Ticket.state = Status.id AND Ticket.priority = Priority.id AND Type.id = Ticket.type AND Category.id = Ticket.category;');
+    SELECT owner.hash as userhash, owner.name as username, ticket.hash as tickethash,
+            assigned.hash as assignedhash, assigned.name as assignedname, category.name as categoryname 
+    FROM user as owner, category, ticket LEFT OUTER JOIN user as assigned
+        ON assigned.hash = ticket.assigned
+    WHERE owner.hash = ticket.owner AND category.hash = ticket.category;');
+
 
 DB::sql('CREATE VIEW displayableactivity AS
-    SELECT activity.id, activity.hash, activity.description, activity.changed, activity.ticket, activity.project, activity.comment,
+    SELECT activity.hash, activity.description, activity.changed, activity.ticket, activity.project, activity.comment,
            user.name as username, user.hash as userhash
     FROM activity, user 
-    WHERE activity.user = user.id;');
+    WHERE activity.user = user.hash;');
 
 
 ?>
