72e03c09c5b0c47caafce0ffaae47f7ea9951fcf
Sascha Ohms <sascha@534f.de>
2013-04-30 07:54:19 +0200

    more setup stuff

--- app/setup.php
+++ app/setup.php
@@ -15,66 +15,33 @@ class Setup {
      * @param type $f3
      */
     public function start() {
-		$f3 = Base::instance();
+	$f3 = Base::instance();
         
-		$f3->set('reqs', $this->_doChecks());        
+	$f3->set('reqs', $this->_doChecks());        
         $this->tpserve();
     }    
     
     /**
      * 
-     * @param type $f3
      * @return type
      */
-	public function install() {
-		$f3 = Base::instance();
+    public function install() {
+	$f3 = Base::instance();
 
-        $admname = $f3->get('POST.name');
-        $admpw = $f3->get('POST.pw');
-        $admemail = $f3->get('POST.email');
-        
-        if($f3->get('POST.dbtype') == 'mysqldb') {
-            $host = $f3->get('POST.sqlhost');
-            $user = $f3->get('POST.sqluser');
-            $pass = $f3->get('POST.sqlpw');
-            $db = $f3->get('POST.sqldb');
-
-            $f3->set('DB', new \DB\SQL('mysql:host=' .$host. ';dbname=' .$db, $user, $pass));
-            
-            require_once '../install/mysql.php';
-            
-            $usr = new User;
-            $usr->registerUser(false, false, $admname, $admpw, $admemail, true);
-            
-            file_put_contents('../data/config.inc.php', "<?php F3::set('DB', new \DB('mysql:host=".$host.";dbname=".$db."', '".$user."', '".$pass."')); ?>");
-            
-            $f3->set('INSTALLED', true);            
-            self::tpserve();            
-        } else {
-            $db = $f3->get('POST.dbname');
-            
-            if(file_exists('../data/'.$db)) {
-                $f3->set('dbexists', true);
-                $this->tpserve();
-                return;
-            }
-
-            $f3->set('DB', new \DB\SQL('sqlite:../data/'.$db));
-            require_once '../install/sqlite.php';
-            
-            $user = new User;
-            if(!$user->registerUser(false, false, $admname, $admpw, $admemail, true)) {
-                $f3->set('usererror', true);
-                self::tpserve();
-                return;
-            }
-            
-            // TODO: file has changed
-            file_put_contents('../data/config.inc.php', "<?php F3::set('DB', new \DB('sqlite:../data/".$db."')); ?>");
-            
-            $f3->set('INSTALLED', true);            
-            self::tpserve();
-        }
+	foreach($f3->get('POST') as $elem => $val) {
+	    $f3->set('SESSION.'.$elem, $val);
+	}
+	
+	if($f3->get('username') && $f3->get('email') &&
+	    $f3->get('password') && $f3->get('passwordrepeat')) {
+	    if($f3->get('password') == $f3->get('passwordrepeat')) {
+		
+	    } else {
+		
+	    }
+	} else {
+	    
+	}
     }           
     
 
@@ -84,29 +51,29 @@ class Setup {
      * @return boolean
      */
     private static function _doChecks() {
-		$f3 = Base::instance();	
-		$error = false;
-		$check = null;
-		
-		$check['sqlite'] = !extension_loaded('pdo_sqlite') ? false : true;
-		$check['mysql'] = !extension_loaded('pdo_mysql') ? false: true;
+	$f3 = Base::instance();	
+	$error = false;
+	$check = null;
 
-		if(!$check['sqlite'] && !$check['mysql'])
-			$error = true;
+	$check['sqlite'] = !extension_loaded('pdo_sqlite') ? false : true;
+	$check['mysql'] = !extension_loaded('pdo_mysql') ? false: true;
 
-		if(is_writeable('../app/')) {
-			$check['config1'] = true;
-		} else {
-			$check['config1'] = false;
-			$error = true;
-		}
+	if(!$check['sqlite'] && !$check['mysql'])
+		$error = true;
 
-		if(!file_exists('../app/config.ini.php')) {
-			$check['config2'] = true;
-		} else {
-			$error = true;
-			$check['config2'] = false;
-		}
+	if(is_writeable('../app/')) {
+		$check['config1'] = true;
+	} else {
+		$check['config1'] = false;
+		$error = true;
+	}
+
+	if(!file_exists('../app/config.ini.php')) {
+		$check['config2'] = true;
+	} else {
+		$error = true;
+		$check['config2'] = false;
+	}
 
         return array('error' => $error, 'checks' => $check);
     }
--- ui/setup.tpl.php
+++ ui/setup.tpl.php
@@ -4,108 +4,178 @@
         <meta charset="UTF-8" />
         <title>Bugtrckr Setup</title>
 
-		<link href="{{@BASE}}/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-		<link href="{{@BASE}}/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-		<link href="{{@BASE}}/css/setup.css" rel="stylesheet" type="text/css" />
+        <link href="{{@BASE}}/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="{{@BASE}}/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="{{@BASE}}/css/setup.css" rel="stylesheet" type="text/css" />
     </head>
 
     <body>
-        <div class="container">
-			<h1>Bugtrckr Setup</h1>
-			<ul class="nav nav-pills">
-				<li class="active"><a href="{{@BASE}}/setup/#requirements" data-toggle="tab">1. Requirements</a></li>
-				<li><a href="{{@BASE}}/setup/#database" data-toggle="tab">2. Database</a></li>
-				<li><a href="{{@BASE}}/setup/#admin" data-toggle="tab">3. Admin</a></li>
-			</ul>
-			<div class="tab-content">
-				<div class="tab-pane active" id="requirements">
-					<table class="table table-bordered">
-						<tr>
-							<th>Requirement</th>
-							<th>Result</th>
-						</tr>
-						<tr>
-							<td>MySQL</td>
-							<td>
-								<check if="{{@reqs.checks.mysql}}">
-									<true><span class="label label-success">avilable</span></true>
-									<false><span class="label label-important">unavailable</false>
-								</check>
-							</td>
-						</tr>
-						<tr>
-							<td>SQLite</td>
-							<td>
-								<check if="{{@reqs.checks.sqlite}}">
-									<true><span class="label label-success">avilable</span></true>
-									<false><span class="label label-important">unavailable</false>
-								</check>
-							</td>
-						</tr>
-						<tr>
-							<td><em>app</em> folder writeable</td>
-							<td>
-								<check if="{{@reqs.checks.config1}}">
-									<true><span class="label label-success">yes</span></true>
-                                    <false><span class="label label-important">no</false>
-								</check>
-							</td>
-						</tr>
-						<tr>
-							<td><em>config.ini.php</em> existing</td>
-							<td>
-								<check if="{{@reqs.checks.config1}}">
-									<true><span class="label label-success">no</span></true>
-									<false><span class="label label-important">yes</false>
-								</check>
-							</td>
-						</tr>
-					</table>
-					<check if="{{@reqs.error}}">
-						<true>
+	<form action="{{@BASE}}/setup" method="post">
+	    <div class="container">
+		<h1>Bugtrckr Setup</h1>
+		<ul class="nav nav-pills">
+		    <li class="active"><a href="{{@BASE}}/setup/#requirements" data-toggle="tab">1. Requirements</a></li>
+		    <check if="{{!@reqs.error}}">
+			<li><a href="{{@BASE}}/setup/#database" data-toggle="tab">2. Database</a></li>
+			<li><a href="{{@BASE}}/setup/#admin" data-toggle="tab">3. Admin</a></li>
+		    </check>
+		</ul>
+		<div class="tab-content">
+		    <div class="tab-pane active" id="requirements">
+			<table class="table table-bordered">
+			    <tr>
+				<th>Requirement</th>
+				<th>Result</th>
+			    </tr>
+			    <tr>
+				<td>MySQL</td>
+				<td>
+				    <check if="{{@reqs.checks.mysql}}">
+					<true><span class="label label-success">avilable</span></true>
+					<false><span class="label label-important">unavailable</false>
+				    </check>
+				</td>
+			    </tr>
+			    <tr>
+				<td>SQLite</td>
+				<td>
+				    <check if="{{@reqs.checks.sqlite}}">
+					<true><span class="label label-success">avilable</span></true>
+					<false><span class="label label-important">unavailable</false>
+				    </check>
+				</td>
+			    </tr>
+			    <tr>
+				<td><em>app</em> folder writeable</td>
+				<td>
+				    <check if="{{@reqs.checks.config1}}">
+					<true><span class="label label-success">yes</span></true>
+					<false><span class="label label-important">no</false>
+				    </check>
+				</td>
+			    </tr>
+			    <tr>
+				<td><em>config.ini.php</em> existing</td>
+				<td>
+				    <check if="{{@reqs.checks.config1}}">
+					<true><span class="label label-success">no</span></true>
+					<false><span class="label label-important">yes</false>
+				    </check>
+				</td>
+			    </tr>
+			</table>
+			<check if="{{@reqs.error}}">
+			    <true>
+				<p class="alert alert-danger">Please fix above errors. You also need at least one DB engine.</p>
+			    </true>
+			    <false>
+				<p class="alert alert-success">You meet all requirements. Let's continue.</p>
+				<hr/>
+				<a href="{{@BASE}}/setup/#dataabse" data-toggle="tab" class="btn btn-success">Next step &raquo;</a>
+			    </false>
+			</check>
+		    </div>
+		    <div class="tab-pane" id="database">
+			<div class="row">
+			    <div class="span3 well">
+				<check if="{{@reqs.checks.mysql}}">
+				    <true>
+					<input type="text" name="sqlusername" placeholder="User name" /><br/>
+					<input type="password" name="sqlpassword" placeholder="Password" /><br/>
+					<input type="password" name="sqlpasswordrep" placeholder="Repeat password" /><br/>
+					<input type="text" name="sqlserver" placeholder="Server" /><br/>
+					<a href="#" class="btn pull-right"><i class="icon-ok"></i> Validate</a>
+				    </true>
+				    <false>
+					<p>MySQL is not available</p>
+				    </false>
+				</check>
+			    </div>
+			    <div class="span8">
+				<check if="{{@reqs.checks.sqlite}}">
+				    <true>
+					<check if="{{@reqs.checks.mysql}}">
+					    <true>
+						<p class="alert alert-info">Skipping this step is possible!</p>
+						<p>Please enter your MySQL credentials on the left.</p>
 
-						</true>
-						<false>
-							<p class="alert alert-success">You meet all requirements. Let's continue.</p>
-							<a href="{{@BASE}}/setup/#dataabse" data-toggle="tab" class="btn btn-success">Next step &raquo;</a>
-						</false>
+						<p><strong>Hint: </strong>Since SQLite is available on your system, you could skip this step 
+							by leaving the fields on the left blank. The setup will create a random-named SQLite 
+							database. You can migrate this database to MySQL later.</p>
+						<p>If you like to use Bugtrckr in a productive environment, you should 
+						consider MySQL!</p>
+					    </true>
+					    <false>
+						<p>Please enable MySQL and reload the page, or just continue to use SQLite instead.</p>
+						<p>The setup will create a random-named SQLite 
+						    database. You can migrate this database to MySQL later.</p>
+					    </false>
 					</check>
-				</div>
-				<div class="tab-pane" id="database">
-					<div class="row">
-						<div class="span3 well">
-							<input type="text" name="username" placeholder="User name" /><br/>
-							<input type="password" name="password" placeholder="Password" /><br/>
-							<input type="password" name="passwordrep" placeholder="Repeat password" /><br/>
-							<input type="text" name="server" placeholder="Server" /><br/>
-							<a href="#" class="btn pull-right"><i class="icon-ok"></i> Validate</a>
-						</div>
-						<div class="span8">
-							<check if="{{@reqs.checks.sqlite}}">
-								<true>
-									<p class="alert alert-info">Skipping this step is possible!</p>
-									<p>Please enter your MySQL credentials on the left.</p>
-
-									<p><strong>Hint: </strong>Since SQLite is available on your system, you could skip this step 
-										by leaving the fields on the left blank. The setup will create a random-named SQLite 
-										database. You can also migrate your existing SQLite DB later.</p>
-									<p>If you like to use Bugtrckr in a productive environment, you should 
-										consider MySQL!</p>
-								</true>
-								<false>
-									<p>Please enter your MySQL credentials on the left. The setup will validate thes data later.</p>	
-								</false>
-							</check>
-						</div>
-					</div>
-					<a href="{{@BASE}}/setup/#dataabse" data-toggle="tab" class="btn btn-success">Next step &raquo;</a>
-				</div>
-				<div class="tab-pane" id="admin">
-					
-				</div>
+				    </true>
+				    <false>
+					<p>Please enter your MySQL credentials on the left.</p>
+					<p><strong>Hint:</strong> SQLite is not available on your server. If you just want to test 
+					    Bugtrckr, we recommend enabling it, because there's no need for credentials and you just 
+					    have to delete the database file.</p>
+				    </false>
+				</check>
+			    </div>
 			</div>
+			<hr/>
+			<a href="{{@BASE}}/setup/#dataabse" data-toggle="tab" class="btn btn-success">Next step &raquo;</a>
+		    </div>
+		    <div class="tab-pane" id="admin">
+			<div class="row">
+			    <div class="span5">
+				<div class="form-horizontal">
+				    <div class="control-group">
+					<label class="control-label">User name</label>
+					<div class="controls">
+					    <input type="text" name="username" placeholder="User name" />
+					</div>
+				    </div>
+				    <div class="control-group">
+					<label class="control-label">Email</label>
+					<div class="controls">
+					    <input type="text" name="email" placeholder="Email" />
+					</div>
+				    </div>
+				    <div class="control-group">
+					<label class="control-label">Password</label>
+					<div class="controls">
+					    <input type="password" name="password" placeholder="Password" />
+					</div>
+				    </div>	
+				    <div class="control-group">
+					<label class="control-label">Repeat password</label>
+					<div class="controls">
+					    <input type="password" name="passwordrepeat" placeholder="Repeat password" />
+					</div>
+				    </div>	
+				</div>
+			    </div>
+			    <div class="span1"></div>
+			    <div class="span5">
+				<p class="alert alert-info">Almost done!</p>
+
+				<p>Create your own account.</p>
+
+				<p>This account will have admin rights, which means 
+				    you have access to all projects without any restrictions.</p>
+			    </div>
+			</div>
+			<hr/>
+			<input type="submit" value="Installieren" class="btn btn-primary" />
+		    </div>
 		</div>
-		<script src="{{@BASE}}/js/jquery.js"></script>
-		<script src="{{@BASE}}/js/bootstrap.min.js"></script>
+	    </div>
+	</form>
+        <script src="{{@BASE}}/js/jquery.js"></script>
+        <script src="{{@BASE}}/js/bootstrap.min.js"></script>
+	<script>
+	$(document).ready(function() {
+	    
+	});
+	</script>
     </body>
 </html>
