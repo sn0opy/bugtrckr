9c8b1aefcac3d1b8fcc65c192f8a9bf19981d995
hanspolo <ph.hanspolo@googlemail.com>
2014-02-09 14:50:03 +0100

    Revert "Using new f3 methods for routes and global config"
    This reverts commit a11b8750ebe302af50cf67647914315fe4c3463f.

--- public/index.php
+++ public/index.php
@@ -12,28 +12,80 @@
 
 $start = microtime();
 
-$f3 = require __DIR__ . '/../lib/base.php';
+$app = require '../lib/base.php';
 
 // really dirty, but works great
-if(file_exists('../setup.cfg'))
+if(file_exists('../data/config.inc.php'))
     include '../data/config.inc.php';
-elseif($f3->exists('POST.email'))
-    $f3->mock('POST /setup');
+elseif($app->exists('POST.email'))
+	$app->mock('POST /setup');
 else
-    $f3->mock('GET /setup');
+	$app->mock('GET /setup');
+	
 
-$f3->setup('../setup.cfg');
-$f3->setup('../routes.cfg');
+$app->set('CACHE', false);
+$app->set('DEBUG', 3);
+$app->set('GUI','../gui/'); 
+$app->set('AUTOLOAD', '../inc/');
+$app->set('TEMP', '../temp/');
+$app->set('LOCALES','../lang/');
+$app->set('LANGUAGE', 'en'); // currently forcing to German
 
-$f3->set('getPermission', function($permission) {
+$app->set('getPermission', function($permission) {
     $helper = new \misc\helper();
     return $helper->getPermission($permission);
 });
 
-$f3->run();
+$app->route('GET /', 'misc\main->start');
+$app->route('GET /roadmap', '\controllers\milestone->showRoadmap');
+$app->route('GET /timeline', '\controllers\timeline->showTimeline');
+$app->route('GET /tickets', '\controllers\ticket->showTickets');
+$app->route('GET /ticket/@hash', '\controllers\ticket->showTicket');
+$app->route('GET /user/@name', '\controllers\user->showUser');
+$app->route('GET /user/new', '\controllers\user->showUserRegister');
+$app->route('GET /user/login', '\controllers\user->showUserLogin');
+$app->route('GET /milestone/@hash', '\controllers\milestone->showMilestone');
+$app->route('GET /project/add', '\controllers\project->showAddProject');
+$app->route('GET /project/settings', '\controllers\project->showProjectSettings');
+$app->route('GET /project/settings/role/@hash', '\controllers\project->showProjectSettingsRole');
+$app->route('GET /project/settings/role/add', '\controllers\project->showAddRole');
+$app->route('GET /project/settings/milestone/@hash', '\controllers\project->showProjectSettingsMilestone');
+$app->route('GET /project/settings/milestone/add', '\controllers\project->showAddMilestone');
+$app->route('GET /project/settings/category/add', '\controllers\project->showAddCategory');
+$app->route('GET /project/settings/category/edit/@hash', '\controllers\project->showEditCategory');
+$app->route('GET /project/settings/role/delete/@hash', '\controllers\project->deleteRole');
+$app->route('GET /project/settings/milestone/delete/@hash', '\controllers\milestone->deleteMilestone');
+$app->route('GET /project/settings/category/delete/@hash', '\controllers\project->deleteCategory');
+$app->route('GET /wiki/@title', '\controllers\wiki->showEntry');
+$app->route('GET /wiki', '\controllers\wiki->showEntry');
+$app->route('GET /wiki/discussion/@hash', '\controllers\wiki->showDiscussion');
 
-$f3->clear('SESSION.SUCCESS');
-$f3->clear('SESSION.FAILURE');
+$app->route('GET /project/select/@project', 'misc\main->selectProject');
+$app->route('GET /user/logout', '\controllers\user->logoutUser');
+$app->route('POST /search', '\controllers\ticket->showTickets');
+$app->route('POST /user/login', '\controllers\user->loginUser');
+$app->route('POST /user/new', '\controllers\user->registerUser');
+$app->route('POST /ticket', '\controllers\ticket->addTicket');
+$app->route('POST /ticket/@hash', '\controllers\ticket->editTicket');
+$app->route('POST /project/add', '\controllers\project->projectAdd');
+$app->route('POST /project/settings/member/setrole', '\controllers\project->projectSetRole');
+$app->route('POST /project/settings/category/add', '\controllers\project->addEditCategory');
+$app->route('POST /project/settings/category/edit', '\controllers\project->addEditCategory');
+$app->route('POST /project/settings/role/edit', '\controllers\project->addEditRole');
+$app->route('POST /project/settings/main/edit', '\controllers\project->projectEditMain');
+$app->route('POST /project/settings/milestone/edit', '\controllers\milestone->addEditMilestone');
+$app->route('POST /project/settings/member/add', '\controllers\project->projectAddMember');
+$app->route('POST /project/setttings/member/delete', '\controllers\project->projectDelMember');
+$app->route('POST /wiki', '\controllers\wiki->editEntry');
+$app->route('POST /wikidiscussion', '\controllers\wiki->addDiscussion');
+
+$app->route('GET /setup', '\misc\setup->start');
+$app->route('POST /setup', '\misc\setup->install');
+
+$app->run();
+
+$app->clear('SESSION.SUCCESS');
+$app->clear('SESSION.FAILURE');
 
 $end = microtime();
 
